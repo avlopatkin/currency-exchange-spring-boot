a1c87c8ad2b5f9c7269b4b7fcfcca01c20d5d7ca
andreilopatkin <andrew4635@yandex.ru>
2025-05-15 18:03:42 +0300

    getOne

--- src/main/java/org/example/currency/currencyexchangespringboot/service/CurrencyService.java
+++ src/main/java/org/example/currency/currencyexchangespringboot/service/CurrencyService.java
@@ -3,7 +3,12 @@ package org.example.currency.currencyexchangespringboot.service;
 import lombok.RequiredArgsConstructor;
 import org.example.currency.currencyexchangespringboot.entity.Currency;
 import org.example.currency.currencyexchangespringboot.repository.CurrencyRepository;
+import org.example.currency.currencyexchangespringboot.rest.CurrencyDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +19,14 @@ public class CurrencyService {
     public void create(Currency currency) {
         Currency entity = new Currency(currency.getCode(), currency.getSign());
         currencyRepository.save(entity);
+    }
+
+    public CurrencyDto getOne(Long id) {
+        Optional<Currency> currencyOptional = currencyRepository.findById(id);
+        if (currencyOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Currency not found");
+        }
+        Currency currency = currencyOptional.get();
+        return new CurrencyDto(currency.getId(), currency.getCode(), currency.getSign());
     }
 }
